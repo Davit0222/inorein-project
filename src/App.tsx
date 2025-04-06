1a4e2716f129cb06252ebedcfba09ba50db67103
Davit0222 <110390733+Davit0222@users.noreply.github.com>
2025-04-06 23:06:18 +0400

    Update App.tsx
    using map for section2

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,21 @@ import Section3 from "./components/Section3";
 import ShowMovieCard from "./components/ShowMovieCard";
 import { slides } from "./data/caruselData.json";
 
+import sectionTitles from "./data/sectionData.json";
 function App() {
 	return (
 		<div className="app">
 			<Header />
 			<Carusel data={slides} />
 			<Section1 />
-			<Section2 name="Copa del Rey 2024/2025" />
-			<Section2 name="PREMIERES" />
-			<Section2 name="Korean Series" />
-			<Section2 name="Free Access" />
-			<Section2 name="Now Cheaper" />
-			<Section2 name="Now in the Subscription" />
-			<Section2 name="Top Watched on MEGOGO" />
-			<Section2 name="Best by IMDB Rating" />
-			<Section2 name="HBO Series" />
-			<Section2 name="Choose by Genre" />
-			<ShowMovieCard name="Premieres" />
-			<ShowMovieCard name="Copa del Rey 2024/2025" />
-			<ShowMovieCard name="PREMIERES" />
-			<ShowMovieCard name="Korean Series" />
-			<ShowMovieCard name="Free Access" />
-			<ShowMovieCard name="Now Cheaper" />
-			<ShowMovieCard name="Now in the Subscription" />
-			<ShowMovieCard name="Top Watched on MEGOGO" />
-			<ShowMovieCard name="Best by IMDB Rating" />
-			<ShowMovieCard name="HBO Series" />
+			{sectionTitles.csectionTitles.map((title) => (
+				<Section2 key={title} name={title} />
+			))}
+
+			{sectionTitles.csectionTitles.map((title) => (
+				<ShowMovieCard key={title} name={title} />
+			))}
+
 			<ChooseByGenre name="Choose by Genre" />
 			<Section3 />
 			<Footer />
